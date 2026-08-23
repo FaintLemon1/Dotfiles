f1bd9c326d5019044f9f713e4e93ab614c68191e
FaintLemon1 <injusticiacesarito@gmail.com>
2026-08-23 11:46:23 -0600

    change locate from hardware-config :)

--- nixos/hosts/mamalona/configuration.nix
+++ nixos/hosts/mamalona/configuration.nix
@@ -6,7 +6,7 @@
 
 {
 	imports = [ # Include the results of the hardware scan.
-      ../../hardware-configuration.nix
+      ./hardware-configuration.nix
       ../../modules/nixos/gaming.nix
       ./graphics.nix #for usage of nvidia cardgraphics
       ../../modules/nixos/shell.nix
